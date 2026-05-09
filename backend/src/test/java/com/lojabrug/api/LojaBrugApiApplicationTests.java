388db116d4674ea6b34dba81fe66397bc51194a9
BrunoBrug <brunobrugnerottodelara@gmail.com>
2026-05-08 21:43:41 -0300

    feat: scaffold Spring Boot API

---
+++ backend/src/test/java/com/lojabrug/api/LojaBrugApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lojabrug.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaBrugApiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
